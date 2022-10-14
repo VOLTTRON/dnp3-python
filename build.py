c4e0fc01006ed5c839f1c26ae0884cb148ef63de
Kefei Mo <kefeimo@gmail.com>
2022-10-14 15:13:13 -0500

    able to build functional wheel using python setup.py bdist_wheel

---
+++ build.py
@@ -0,0 +1,81 @@
+import os
+import shutil
+import sys
+
+from distutils.command.build_ext import build_ext
+from distutils.core import Distribution
+from distutils.core import Extension
+from distutils.errors import CCompilerError
+from distutils.errors import DistutilsExecError
+from distutils.errors import DistutilsPlatformError
+
+
+with_extensions = True
+
+extensions = []
+if with_extensions:
+    extensions = [
+        Extension("pydnp3", ["src/pydnp3.cpp"]),
+        # Extension("pendulum.parsing._iso8601", ["pendulum/parsing/_iso8601.c"]),
+    ]
+
+
+class BuildFailed(Exception):
+
+    pass
+
+
+class ExtBuilder(build_ext):
+    # This class allows C extension building to fail.
+
+    built_extensions = []
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except (DistutilsPlatformError, FileNotFoundError):
+            print(
+                "  Unable to build the C extensions, "
+                "Pendulum will use the pure python code instead."
+            )
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError):
+            print(
+                '  Unable to build the "{}" C extension, '
+                "Pendulum will use the pure python version of the extension.".format(
+                    ext.name
+                )
+            )
+
+
+def build(setup_kwargs):
+    """
+    This function is mandatory in order to build the extensions.
+    """
+    distribution = Distribution({"name": "pendulum", "ext_modules": extensions})
+    distribution.package_dir = "pendulum"
+
+    cmd = ExtBuilder(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        if not os.path.exists(output):
+            continue
+
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+    return setup_kwargs
+
+
+if __name__ == "__main__":
+    build({})
+
